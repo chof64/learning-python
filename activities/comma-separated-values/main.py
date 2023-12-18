e6c344596a0a7ed711d1e717c337c7f8ca323cba
Chad Fernandez <contact@chadfernandez.me>
2023-12-18 16:19:46 +0800

    updated to fix multi line input issues

--- activities/comma-separated-values/main.py
+++ activities/comma-separated-values/main.py
@@ -6,24 +6,22 @@
 # We use the with statement to open the file. This is the preferred way as it
 # will automatically close the file for us when we are done with it.
 with open("data.txt", "r") as f:
-  contents = f.read()
+  contents = f.readlines()
 
 
-# SPLIT STRING
-# We split the string into a list of strings. The delimiter is ", " (comma and a space).
-contents = contents.split(", ")
-
+items = []
 
 # STRIP NEWLINE CHARACTERS
 # Each element in the list has a newline character at the end. We can use the
 # strip() method to remove the newline character.
 for i in range(len(contents)):
-  contents[i] = contents[i].strip()
+  for thing in contents[i].strip().split(", "):
+    items.append(thing)
 
 
 # SORT LIST
 # Sort the list alphabetically.
-contents.sort()
+items.sort()
 
 
 # OPEN/CREATE OUTPUT FILE
@@ -34,4 +32,4 @@ contents.sort()
 # We use the join() method to join the elements in the list into a string. The
 # delimiter is ", " (comma and a space). This converts the list into a string.
 with open("output.txt", "w") as f:
-  f.write(", ".join(contents))
+  f.write(", ".join(items))
